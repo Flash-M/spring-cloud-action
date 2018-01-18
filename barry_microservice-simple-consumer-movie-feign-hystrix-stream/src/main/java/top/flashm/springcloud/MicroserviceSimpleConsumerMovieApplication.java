ffe0cbbdb815da8a5baa724ab034cf3cb0b53676
FlashM <feixiang.mei@gmail.com>
2018-01-18 10:44:56 +0800

    feign-hystrix-stream

--- barry_microservice-simple-consumer-movie-feign-hystrix-stream/src/main/java/top/flashm/springcloud/MicroserviceSimpleConsumerMovieApplication.java
+++ barry_microservice-simple-consumer-movie-feign-hystrix-stream/src/main/java/top/flashm/springcloud/MicroserviceSimpleConsumerMovieApplication.java
@@ -2,20 +2,16 @@ package top.flashm.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class MicroserviceSimpleConsumerMovieApplication {
 	
-/*	@Bean
-	@LoadBalanced//使用Ribbon实现负载均衡
-	public RestTemplate returnRestTemplate(){
-		return new RestTemplate();
-	}*/
-
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceSimpleConsumerMovieApplication.class, args);
 	}
